bd1001be9945a7da90a85e27b5d1a74d50ea15e1
Parrabola <maxim.keip@gmail.com>
2020-04-24 15:48:29 +0300

    initial commit

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ru, en, gb etc.")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart browser for test..")
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    time.sleep(10)
+    print("\nquit browser..")
+    browser.quit()
+
+
+
