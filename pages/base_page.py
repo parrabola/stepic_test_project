4f6599ff77e1b4e5b4969edc9af0dbec495fbec8
Parrabola <maxim.keip@gmail.com>
2020-05-13 23:43:26 +0300

    add annotation for browser

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,9 @@
+from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser: webdriver, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
