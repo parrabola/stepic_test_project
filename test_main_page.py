8dee952ccbec7268fe5adb800d749f446faaca8e
Parrabola <maxim.keip@gmail.com>
2020-05-13 23:42:26 +0300

    add test_should_be_login_page() func

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
@@ -10,6 +12,15 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
