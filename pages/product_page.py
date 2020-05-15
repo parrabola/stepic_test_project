cf145df5c775ad6c525f3a75801da62317be2257
Parrabola <maxim.keip@gmail.com>
2020-05-16 00:27:23 +0300

    initial commit

---
+++ pages/product_page.py
@@ -0,0 +1,24 @@
+import time
+
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+        self.prooduct_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        self.product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+
+    def add_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        add_button.click()
+
+    def costs_must_match(self):
+        assert self.browser.find_element(
+            *ProductPageLocators.PRICE_BASKET_FROM_ALERT_SUCCESS).text == self.product_price
+
+    def names_must_match(self):
+        assert self.browser.find_element(
+            *ProductPageLocators.PRODUCT_NAME_FROM_ALERT_SUCCESS).text == self.prooduct_name
