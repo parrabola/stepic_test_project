7416240f8d912142255ee3079a61225f8337606b
Parrabola <maxim.keip@gmail.com>
2020-05-18 01:48:22 +0300

    remove sleep()

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
+
 import pytest
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
@@ -9,16 +11,12 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(scope="function")
-def browser(request):
+def browser(request) -> webdriver:
     print("\nstart browser for test..")
     user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
     yield browser
-    time.sleep(10)
     print("\nquit browser..")
     browser.quit()
-
-
-
