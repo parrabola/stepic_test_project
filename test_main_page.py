8dbbb0865b9103deb4f81ff1155504ece0e9308f
Parrabola <maxim.keip@gmail.com>
2020-05-19 22:35:25 +0300

    add test_guest_cant_see_product_in_basket_opened_from_main_page(), test_guest_can_see_product_in_basket_opened_from_main_page() funcs

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,6 @@
+import pytest
+
+from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
@@ -26,3 +29,24 @@ def test_should_be_login_page(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.basket_is_empty()
+    basket_page.should_be_empty_basket_message()
+
+
+@pytest.mark.xfail
+def test_guest_can_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_goods()
+    basket_page.should_be_no_empty_basket_message()
