d34452dc88eb5aa4bf3948e59d81a5fa21cbee82
Parrabola <maxim.keip@gmail.com>
2020-05-19 22:36:11 +0300

    add test_guest_cant_see_product_in_basket_opened_from_product_page(), def test_guest_can_see_product_in_basket_opened_from_product_page() funcs

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,6 @@
 import pytest
+
+from .pages.basket_page import BasketPage
 from .pages.locators import ProductPageLocators
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
@@ -65,3 +67,22 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.go_to_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.basket_is_empty()
+    basket_page.should_be_empty_basket_message()
+
+
+@pytest.mark.xfail
+def test_guest_can_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_no_empty_basket_message()
+    basket_page.should_be_goods()
